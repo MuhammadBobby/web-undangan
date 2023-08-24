a0a0b3044331acaed8d858020606916ab3ee79f2
Muhammad Bobby <muhammadbobbyoktaviano@gmail.com>
2023-08-24 16:44:22 +0700

    Menyelesaikan Web Undangan display

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ window.addEventListener("scroll", function () {
     nav.classList.remove("sticky");
   }
 });
+
+// untuk disable masuk ke sript Google spreedsheed
+window.addEventListener("load", function () {
+  const form = document.getElementById("my-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      alert("Berhasil Mengonfirmasi Kehadiran Anda!");
+    });
+  });
+});
